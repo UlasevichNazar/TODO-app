b7fe4f41f4be89e10408a495f074478281ba4f38
Ulasevich_Nazar <nazar.ulasevich1@gmail.com>
2023-05-10 11:15:51 +0300

    feat: create task_update

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from tasks.views import TaskList, TaskDetail, TaskCreate
+from tasks.views import TaskList, TaskDetail, TaskCreate, TaskUpdate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='task_list'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task_detail'),
     path('task-create/', TaskCreate.as_view(), name='task_create'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task_update'),
 
 ]
