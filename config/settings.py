5d27aad349276b887a2eb36429fc13983b3639de
Ulasevich_Nazar <nazar.ulasevich1@gmail.com>
2023-05-11 11:24:30 +0300

    feat: create todo

--- config/settings.py
+++ config/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 # STATIC AND MEDIA
 ###############################################
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- registration_and_login/form.py
+++ registration_and_login/form.py
@@ -1,9 +1,16 @@
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+
 User = get_user_model()
 
 
 class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(label="Никнейм", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.CharField(label="Email", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label="Пароль", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label="Повтор пароля", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ["username", "email", "password1", "password2"]
